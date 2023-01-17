d16bfea997332d545ece39e482d9234ed33b42f8
DdimaPos <postoroncadumitru@gmail.com>
2023-01-17 18:45:23 +0200

    new structure of objects

--- js/script.js
+++ js/script.js
@@ -1,104 +1,3 @@
-/*window.addEventListener('load', () =>{
-    const catForm = document.querySelector('#new-category-form');
-    const catInput = document.querySelector('#new-category-input');
-    catForm.addEventListener('submit', (ev) =>{
-        ev.preventDefault();
-        const newCat = catInput.value;
-        if (!newCat) {
-            alert('You have typed nothing!');
-            return;
-        }
-        catList.insertAdjacentHTML('afterbegin',
-        `<details class="spoiler">
-        <summary>
-            <div class="list" id="todo-list">
-                <div class="todo-content">
-                    <input id="new-category-val" type="text" value="${newCat}" readonly/>
-                </div>
-                <div class="actions">
-                    <button class="edit">Edit</button>
-                    <button class="delete">Delete</button>
-                </div>
-            </div>
-        </summary>
-        <form id="new-exercise-form">
-            <input id="new-exercise-input" placeholder="e.g push ups" type="text" />
-            <input id="new-exercise-submit" type="submit" value="Add exercise">
-        </form>
-        <div class="ex__list" id="ex-todo-list"></div>
-        </details>`
-        );
-        catInput.value = null;
-            //delete and edit categories
-        const catEl = catList.querySelector('.spoiler');
-        const catEditEl = catList.querySelector('summary').querySelector('.edit');
-        const catDeleteEl = catList.querySelector('summary').querySelector('.delete');
-        const catInputEl = catList.querySelector('#new-category-val');
-        catEditEl.addEventListener('click', ()=>{
-            if (catEditEl.innerText.toLowerCase() == "edit") {
-                catInputEl.removeAttribute("readonly");
-                catInputEl.focus();
-                catEditEl.innerText = "Save";
-            } else {
-                catInputEl.setAttribute("readonly", "readonly");
-                catEditEl.innerText = "Edit";
-            }
-        });
-        catDeleteEl.addEventListener('click', () =>{
-            catList.removeChild(catEl);
-        });
-            //
-        console.log(catEditEl);
-        console.log(catList);
-        const exForm = document.querySelector('#new-exercise-form');
-        const exInput = document.querySelector('#new-exercise-input');
-        const exList = document.querySelector('.ex__list');
-        exForm.addEventListener('submit', (ev) => {
-            ev.preventDefault();
-            const newEx = exInput.value;
-            if (!newEx) {
-                alert('You have typed nothing!');
-                return;
-            } 
-            exList.insertAdjacentHTML('afterbegin',
-            `<div class="exercise">    
-                <label >
-                    <input type="checkbox"/>
-                    <span class="check"></span>
-                </label>
-                <div class="todo-content ">
-                    <input id="new-exercise-val" value="${newEx}" type="text"  readonly/>
-                </div>
-                <div class="actions">
-                    <button class="edit">Edit</button>
-                    <button class="delete">Delete</button>
-                </div>
-            </div>`
-            );
-            exInput.value = null;
-                //edit and delete exercises
-            const exEl = exList.querySelector('.exercise');
-            const exEditEl = exList.querySelector('.edit');
-            const exDeleteEl = exList.querySelector('.delete');
-            const exInputEl = exList.querySelector('#new-exercise-val');
-            const exCheck = exEl.querySelector('label').querySelector('input');
-            exEditEl.addEventListener('click', ()=>{
-                if (exEditEl.innerText.toLowerCase() == "edit") {
-                    exInputEl.removeAttribute("readonly");
-                    exInputEl.focus();
-                    exEditEl.innerText = "Save";
-                } else {
-                    exInputEl.setAttribute("readonly", "readonly");
-                    exEditEl.innerText = "Edit";
-                }
-            });
-            exDeleteEl.addEventListener('click', () =>{
-                exList.removeChild(exEl);
-            });
-                //      
-        });
-    }); 
-}); */
 window.addEventListener("load", () =>{
     todos=JSON.parse(localStorage.getItem('todos')) || [];
     const catForm = document.querySelector('#new-category-form');
@@ -109,6 +8,7 @@ window.addEventListener("load", () =>{
             content: ev.target.elements.content.value,
             //done: false,
             createdAt: new Date().getTime(),
+            exercises:[],
         }
 
         todos.push(catVal);
@@ -118,10 +18,11 @@ window.addEventListener("load", () =>{
 
         DisplayCat();
     });
+    
     DisplayCat();
 });
 
-function DisplayCat(){
+function DisplayCat(i){
     const catList = document.querySelector('.todo-list');
 
     catList.innerHTML= '';
@@ -129,26 +30,27 @@ function DisplayCat(){
     todos.forEach(catVal => {
         if (!catVal.content) {
             return;
-        }
-        console.log('category');
-        //console.log(todos.indexOf(catVal)); 
+        } 
         const catItem = document.createElement('details');
-        catItem.classList.add('spoiler');
         const summary = document.createElement('summary');
-        const catEl = document.createElement('div');catEl.classList.add('list');
-        const catContent = document.createElement('div');catContent.classList.add('todo-content');
-        const actions = document.createElement('div');actions.classList.add('actions');
-        const edit = document.createElement('button');edit.classList.add('edit');
-        const deleteButton = document.createElement('button');deleteButton.classList.add('delete');
-        const exForm = document.createElement('form');exForm.id = 'new-exercise-form';
-        const exList = document.createElement('div');exList.classList.add('ex__list');
-        exForm.innerHTML=`<input name="content" id="new-exercise-input" placeholder="e.g push ups" type="text" />
-        <input id="new-exercise-submit" type="submit" value="Add exercise">`;
-        catContent.innerHTML = `<input id="new-category-val" type="text" 
-        value="${catVal.content}" readonly/>`;
-        edit.innerHTML = 'EDIT';
-        deleteButton.innerHTML = 'DELETE';
+        const exList = document.createElement('div');
+        const catEl = document.createElement('div');
+        const catContent = document.createElement('div');
+        const actions = document.createElement('div');
+        const edit = document.createElement('button');
+        const deleteButton = document.createElement('button');
+        const exForm = document.createElement('form');
+        const exInput = document.querySelector('#new-exercise-input');
         
+        catItem.classList.add('spoiler');
+        catEl.classList.add('list');
+        catContent.classList.add('todo-content');
+        actions.classList.add('actions');
+        edit.classList.add('edit');
+        deleteButton.classList.add('delete');
+        exForm.id = 'new-exercise-form';        
+        exList.classList.add('ex__list');
+
         catList.appendChild(catItem);
         catItem.appendChild(summary);
         catItem.appendChild(exForm);
@@ -158,51 +60,50 @@ function DisplayCat(){
         catEl.appendChild(actions);
         actions.appendChild(edit);
         actions.appendChild(deleteButton);
-        const exInput = document.querySelector('#new-exercise-input');
+        
+        exForm.innerHTML=`<input name="content" id="new-exercise-input" placeholder="e.g push ups" type="text" />
+        <input id="new-exercise-submit" type="submit" value="Add exercise">`;
+        catContent.innerHTML = `<input id="new-category-val" type="text" 
+        value="${catVal.content}" readonly/>`;
+        edit.innerHTML = 'EDIT';
+        deleteButton.innerHTML = 'DELETE';
         exForm.addEventListener('submit', (ev) =>{
             ev.preventDefault();
-            exVal ={
-                exercise: ev.target.elements.content.value,
+            exVal={
+                ex_name: ev.target.elements.content.value,
                 done: false,
-                createdAt: new Date().getTime(),
             }
     
-            todos.push(exVal);
+            catVal.exercises.push(exVal);
     
             localStorage.setItem('todos', JSON.stringify(todos));
             
             ev.target.reset();
-            DisplayEx(todos.indexOf(catVal));
+            DisplayEx(catVal);
         });
-        DisplayEx(todos.indexOf(catVal));
+        DisplayEx(catVal);
         
     });
 }
 
-function DisplayEx(i) {
+function DisplayEx(catVal) {
     const exList = document.querySelector('.ex__list');
     exList.innerHTML = ' ';
-    todos.forEach(exVal =>{
-        if (!exVal.exercise) {
-            return;
-            //todos.indexOf(exVal)<i
-        }
-        console.log('exercise');
+    console.log(exList);
+    for(let i=0;i<catVal.exercises.length; i++){
+        console.log('exercise created');
 //      console.log(todos.indexOf(exVal));
         const exItem = document.createElement('div');exItem.classList.add('exercise');
         const label = document.createElement('label');
         const input = document.createElement('input');
         const span = document.createElement('span');span.classList.add()
-        input.type = 'checkbox'; input.checked = exVal.done;
+        input.type = 'checkbox'; input.checked = catVal.done;
         const content = document.createElement('div');content.classList.add('todo-content')
         const actions = document.createElement('div');actions.classList.add('actions');
         const edit = document.createElement('button');edit.classList.add('edit');
         const deleteButton = document.createElement('button');deleteButton.classList.add('delete');
 
-        content.innerHTML = `<input id="new-exercise-val"  type="text" 
-        value="${exVal.exercise}" readonly/>`;
-        edit.innerHTML = 'EDIT';
-        deleteButton.innerHTML = 'DELETE';
+       
         exItem.appendChild(label);
         exItem.appendChild(content);
         exItem.appendChild(actions);
@@ -210,8 +111,11 @@ function DisplayEx(i) {
         label.appendChild(span);
         actions.appendChild(edit);
         actions.appendChild(deleteButton);
-        
         exList.appendChild(exItem);
-    });
+         content.innerHTML = `<input id="new-exercise-val"  type="text" 
+        value="${catVal.exercises[i].ex_name}" readonly/>`;
+        edit.innerHTML = 'EDIT';
+        deleteButton.innerHTML = 'DELETE';
+    };
     
 }
